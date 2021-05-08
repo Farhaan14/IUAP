4f83265500b47e4e952755fa46e6d83d26b3a875
Varan Gulati <53681461+VTPrince@users.noreply.github.com>
2021-05-08 21:58:17 +0530

    Update heart_disease_app.py

--- heart_disease_app.py
+++ heart_disease_app.py
@@ -10,6 +10,43 @@ Original file is located at
 import numpy as np
 import pickle
 import streamlit as st
+import pandas as pd
+
+# Security
+#passlib,hashlib,bcrypt,scrypt
+import hashlib
+def make_hashes(password):
+	return hashlib.sha256(str.encode(password)).hexdigest()
+
+def check_hashes(password,hashed_text):
+	if make_hashes(password) == hashed_text:
+		return hashed_text
+	return False
+# DB Management
+import sqlite3 
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+# DB  Functions
+def create_usertable():
+	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
+
+
+def add_userdata(username,password):
+	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+	conn.commit()
+
+def login_user(username,password):
+	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
+	data = c.fetchall()
+	return data
+
+
+def view_all_users():
+	c.execute('SELECT * FROM userstable')
+	data = c.fetchall()
+	return data
+
+
 
 # Load ML model
 model = pickle.load(open('model.pkl', 'rb')) 
@@ -76,22 +113,75 @@ def predict(age,sex,chest_pain,rest_bp,serum_cl,fast_bs,rest_ecg,max_hr,st_dep,e
     else:
         st.text("The patient is likely to have heart disease!")
 def main():
-    age = st.number_input('Age')
-    sex = st.selectbox('Gender',("Male","Female"))
-    chest_pain = st.selectbox('Chest Pain Type',("asymptomatic","typical angina","typical angina","non-anginal pain"))
-    rest_bp = st.number_input("Resting Blood Pressure in mm Hg")
-    serum_cl = st.number_input("Serum Cholestoral in mg/dl")
-    fast_bs = st.selectbox("Fasting Blood Sugar",(False,True))
-    rest_ecg = st.selectbox("Resting ECG Results",("normal","Having ST-T wave abnormality","left ventricular hypertrophy"))
-    max_hr = st.number_input("Maximum Heart Rate")
-    st_dep = st.number_input("ST Depression Induced")
-    ex_ag = st.selectbox("Exercise Induced Angina ",("No","Yes"))
-    slope = st.selectbox("Slope of the Peak Exercise ST Segment",("unsloping","flat","downsloping"))
-    ves_nm = st.selectbox("Number of Vessels Colored by Flourosopy",(0,1,2,3))
-    tha = st.selectbox("Thalassemia",("fixed defect","normal","reversable defect"))
-    
-    if st.button("Predict"):
-        predict(age,sex,chest_pain,rest_bp,serum_cl,fast_bs,rest_ecg,max_hr,st_dep,ex_ag,slope,ves_nm,tha)
+
+	menu = ["Home","Login","SignUp"]
+	choice = st.sidebar.selectbox("Menu",menu)
+
+	if choice == "Home":
+		st.subheader("Home")
+		
+
+	elif choice == "Login":
+		st.subheader("Login Section")
+
+		username = st.sidebar.text_input("User Name")
+		password = st.sidebar.text_input("Password",type='password')
+		if st.sidebar.checkbox("Login"):
+			# if password == '12345':
+			create_usertable()
+			hashed_pswd = make_hashes(password)
+
+			result = login_user(username,check_hashes(password,hashed_pswd))
+			if result:
+
+				st.success("Logged In as {}".format(username))
+
+				task = st.selectbox("Task",["Check Heart","Analytics","Profiles"])
+				if task == "Check Heart":
+					st.subheader("Check Heart")
+					age = st.number_input('Age')
+					sex = st.selectbox('Gender',("Male","Female"))
+					chest_pain = st.selectbox('Chest Pain Type',("asymptomatic","typical angina","typical angina","non-anginal pain"))
+					rest_bp = st.number_input("Resting Blood Pressure in mm Hg")
+					serum_cl = st.number_input("Serum Cholestoral in mg/dl")
+					fast_bs = st.selectbox("Fasting Blood Sugar",(False,True))
+					rest_ecg = st.selectbox("Resting ECG Results",("normal","Having ST-T wave abnormality","left ventricular hypertrophy"))
+					max_hr = st.number_input("Maximum Heart Rate")
+					st_dep = st.number_input("ST Depression Induced")
+					ex_ag = st.selectbox("Exercise Induced Angina ",("No","Yes"))
+					slope = st.selectbox("Slope of the Peak Exercise ST Segment",("unsloping","flat","downsloping"))
+					ves_nm = st.selectbox("Number of Vessels Colored by Flourosopy",(0,1,2,3))
+					tha = st.selectbox("Thalassemia",("fixed defect","normal","reversable defect"))
+					if st.button("Predict"):
+						predict(age,sex,chest_pain,rest_bp,serum_cl,fast_bs,rest_ecg,max_hr,st_dep,ex_ag,slope,ves_nm,tha)
+
+				elif task == "Analytics":
+					st.subheader("Analytics")
+				elif task == "Profiles":
+					st.subheader("User Profiles")
+					user_result = view_all_users()
+					clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
+					st.dataframe(clean_db)
+			else:
+				st.warning("Incorrect Username/Password")
+
+
+
+
+
+	elif choice == "SignUp":
+		st.subheader("Create New Account")
+		new_user = st.text_input("Username")
+		new_password = st.text_input("Password",type='password')
+
+		if st.button("Signup"):
+			create_usertable()
+			add_userdata(new_user,make_hashes(new_password))
+			st.success("You have successfully created a valid Account")
+			st.info("Go to Login Menu to login")
+
+
+
 
 
 
